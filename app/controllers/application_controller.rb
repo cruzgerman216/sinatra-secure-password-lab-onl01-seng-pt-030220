c0f4124990fbc6eb6a46df1d041360767f73e29d
German Cruz <cruzgerman216@yahoo.com>
2020-05-21 06:21:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ end
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to "/success"
+      redirect to "/account"
     else
       redirect to "/failure"
     end
